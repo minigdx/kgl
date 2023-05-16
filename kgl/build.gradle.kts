b10d1d06cb0af15a203a49121023a5bdc4fe3a47
David Wursteisen <david.wursteisen+git@gmail.com>
2023-05-16 10:25:42 +0200

    Add project names for maven publishing

--- kgl/build.gradle.kts
+++ kgl/build.gradle.kts
@@ -1,3 +1,22 @@
 plugins {
     id("com.github.minigdx.gradle.plugin.developer.mpp")
 }
+
+minigdxDeveloper {
+    this.name.set("kgl")
+    this.description.set("Lightweight OpenGL abstraction for Kotlin Multiplatform (fork of https://github.com/gergelydaniel/kgl)")
+    this.projectUrl.set("https://github.com/minigdx/kgl")
+    this.licence {
+        name.set("MIT Licence")
+        url.set("https://github.com/minigdx/kgl/blob/main/LICENSE")
+    }
+    developer {
+        name.set("Daniel Gergely")
+        url.set("https://github.com/gergelydaniel")
+    }
+    developer {
+        name.set("David Wursteisen")
+        email.set("david.wursteisen+minigdx@gmail.com")
+        url.set("https://github.com/dwursteisen")
+    }
+}
